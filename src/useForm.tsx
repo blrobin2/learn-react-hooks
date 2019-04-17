5eeb0d49ad03e2ddf0c117150ccc8374fe892d51
Bennie <brobinson88@gmail.com>
2019-04-17 10:14:00 -0400

    format useForm

--- src/useForm.tsx
+++ src/useForm.tsx
@@ -2,29 +2,31 @@ import { useState } from 'react';
 import { FormElementEvent, InputEvent } from './react-app-env';
 
 export default function useForm<T>(
-  emptyForm: T,
-  callback?: (formData: FormData) => void,
-): {
-  handleChange: (event: InputEvent) => void;
-  handleSubmit: (event: FormElementEvent) => void;
-  values: T;
-} {
-  const [values, setValues] = useState<T>(emptyForm);
+    emptyForm: T,
+    callback?: (formData: FormData) => void,
+):
+    {
+        handleChange: (event: InputEvent) => void;
+        handleSubmit: (event: FormElementEvent) => void;
+        values: T;
+    }
+{
+    const [values, setValues] = useState<T>(emptyForm);
 
-  return {
-    handleChange(event: InputEvent): void {
-      event.persist();
-      setValues((values: T) => ({
-        ...values,
-        [event.target.name]: event.target.value,
-      }));
-    },
-    handleSubmit(event: FormElementEvent): void {
-      event.preventDefault();
-      if (callback) {
-        callback(new FormData(event.target));
-      }
-    },
-    values,
-  };
+    return {
+        handleChange(event: InputEvent): void {
+            event.persist();
+            setValues((values: T): T => ({
+                ...values,
+                [event.target.name]: event.target.value,
+            }));
+        },
+        handleSubmit(event: FormElementEvent): void {
+            event.preventDefault();
+            if (callback) {
+                callback(new FormData(event.target));
+            }
+        },
+        values,
+    };
 }
